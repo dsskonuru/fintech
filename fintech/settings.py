b4aa124231363aada4b2e4979faad035d03414a4
Durga Sai <konurud@gmail.com>
2020-05-23 22:14:49 +0530

    updates allowed hosts

--- fintech/settings.py
+++ fintech/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'rgvv$w-o6a-c+$5!!rt3_unq*=*%z9%vk(7j+blddy3ge%y6(o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['18.223.113.52']
 
 # Application definition
 
